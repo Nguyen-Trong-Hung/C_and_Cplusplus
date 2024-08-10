a53c5ea2dc1e518d048cba7d1d7f847125dcfd9f
Amunra <hungnguyentrong285@gmail.com>
2024-08-10 17:54:17 +0700

    Up

---
+++ inoutput.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+//int = 4byte = 32bit = float, double = 8byte, char = 1 byte
+// kieudulieu: longlong + bên trên
+// Nhapxuat: scanf, printf
+    // printf("%.2f", d); scanf("%f", d);
+// Operator: +-*/%(Toán tử 2 ngôi) = /= *= += -=(Toán tử gán), ++--(Toán tử một ngôi), ==><>=<=!=(Toán tử quan hệ), &&||!(Toán tử logic)
+// Hàm toán học: sqrt, pow, abs, cbrt, ceil, floor
+// if else
+// switch, case
+    // switch (n){case 1:printf("31");break;case 2:printf("28");break;default:printf("Khong hop le");break;}
+int main(){
+    
+}
---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,15 @@
+    int n;
+    scanf("%d", n);
+    switch (n)
+    {
+    case 1:
+        printf("31");
+        break;
+    case 2:
+        printf("28");
+    case 3:
+        printf("31");
+    default:
+        printf("Khong hop le");
+        break;
+    }
