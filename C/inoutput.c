c395105408f5548d646e16d2ca804be95dd21575
Amunra <hungnguyentrong285@gmail.com>
2024-09-14 18:20:50 +0700

    Up

--- C/inoutput.c
+++ C/inoutput.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include <stdlib.h>
 
 //int = 4byte = 32bit = float, double = 8byte, char = 1 byte
 // kieudulieu: longlong + bên trên
@@ -14,29 +15,82 @@
 //      strcmp(char a[], char b[]): So sánh 2 xâu   a>b => 1, a=b => 0
 //      gets(s): Nhập vào, atoll: chuyển xâu(là ký tự số) sang số longlong
 //      strrev(s): Đảo xâu, strlwr(s): strlower, strupr(s): strupper, strcpy(s, a): coppy nội dung của xâu a vào xâu s, strcat(s, a) Nối thêm xâu a vào xâu s
+
+
+// FILE
+// int main(){
+//     FILE *fptr;
+
+//     // // Open a file in writing mode
+//     // fptr = fopen("filename.txt", "w");
+
+//     // // Write some text to the file
+//     // fprintf(fptr, "Hello World!");
+//     // // Append some text to the file
+//     // fprintf(fptr, "\nHi everybody!");
+
+//     // Close the file
+//     // Open a file in read mode
+//     fptr = fopen("filename.txt", "r");
+//     // Store the content of the file
+//     char myString[100];
+
+//     // Read the content and store it inside myString
+//     fgets(myString, 100, fptr);
+
+//     // Print the file content
+//     printf("%s", myString);
+
+//     // Close the file
+//     fclose(fptr);
+// }
+
+// enum Level {
+//   LOW = 1,
+//   MEDIUM,
+//   HIGH
+// };
+
+// int main() {
+//   enum Level myVar = MEDIUM;
+
+//   switch (myVar) {
+//     case 1:
+//       printf("Low Level");
+//       break;
+//     case 2:
+//       printf("Medium level");
+//       break;
+//     case 3:
+//       printf("High level");
+//       break;
+//   }
+//   return 0;
+// }
+
+
 int main(){
-    FILE *fptr;
+    // int *ptr1 = malloc(size);
+    // int *ptr2 = calloc(amount, size);
+    // int *ptr2 = realloc(ptr1, size);
+    int *ptr1, *ptr2;
 
-    // // Open a file in writing mode
-    // fptr = fopen("filename.txt", "w");
+    // Allocate memory
+    ptr1 = malloc(4);
 
-    // // Write some text to the file
-    // fprintf(fptr, "Hello World!");
-    // // Append some text to the file
-    // fprintf(fptr, "\nHi everybody!");
+    // Attempt to resize the memory
+    ptr2 = realloc(ptr1, 8);
 
-    // Close the file
-    // Open a file in read mode
-    fptr = fopen("filename.txt", "r");
-    // Store the content of the file
-    char myString[100];
+    // Check whether realloc is able to resize the memory or not
+    if (ptr2 == NULL) {
+    // If reallocation fails
+    printf("Failed. Unable to resize memory");
+    } else {
+    // If reallocation is sucessful
+    printf("Success. 8 bytes reallocated at address %p \n", ptr2);
+    ptr1 = ptr2;  // Update ptr1 to point to the newly allocated memory
+    }
 
-    // Read the content and store it inside myString
-    fgets(myString, 100, fptr);
-
-    // Print the file content
-    printf("%s", myString);
-
-    // Close the file
-    fclose(fptr);
+    // Free allocated memory
+    free(ptr1);
 }
